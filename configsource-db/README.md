c82db8aff00ce935df5c29db496acd6979f96faa
Phillip Kruger <phillip.kruger@gmail.com>
2019-01-23 15:41:49 +0200

    "Added events to database config source"
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- configsource-db/README.md
+++ configsource-db/README.md
@@ -6,6 +6,8 @@ A eclipse microprofile config extension which uses a database as source.
 ## Overview
 The eclipse microprofile config framework is a simple yet powerful configuration framework for Java EE. But most implementations only provide the system/env properties or property files as configuration source. This small library provides an ConfigSource implementation which reads the values from the default datasource. For performance reasons, the config values are cached.
 
+> This config source expects JNDI & JDBC to be available. The datasource are looked up
+
 ## Usage
 ```xml
         <dependency>
@@ -22,6 +24,16 @@ Currently there are 5 values you can configure, either through Java system prope
 * **configsource.db.key-column** name of the column containing the key, default value is "key"
 * **configsource.db.value-column** name of the column containing the value, default value is "value"
 * **configsource.db.validity** how long to cache values (in seconds), default is 30s
+
+### Events
+
+This config source fires CDI Events on changes
+
+Read more about [Config Events](https://github.com/microprofile-extensions/config-ext/blob/master/config-events/README.md)
+
+You can disable this with the `configsource.db.notifyOnChanges` property:
+
+    configsource.db.notifyOnChanges=false
 
 ## Links
 * https://microprofile.io/project/eclipse/microprofile-config
--- configsource-db/src/main/java/org/microprofileext/config/source/db/DatasourceConfigSource.java
+++ configsource-db/src/main/java/org/microprofileext/config/source/db/DatasourceConfigSource.java
@@ -7,25 +7,28 @@ import java.util.logging.Level;
 import org.microprofileext.config.source.base.EnabledConfigSource;
 
 import lombok.extern.java.Log;
+import org.microprofileext.config.event.ChangeEvent;
+import org.microprofileext.config.event.ChangeEventNotifier;
+import org.microprofileext.config.event.Type;
 
 @Log
 public class DatasourceConfigSource extends EnabledConfigSource {
-
-    private Map<String, TimedEntry> cache = new ConcurrentHashMap<>();
+    private static final String NAME = "DatasourceConfigSource";
+    
+    private final Map<String, TimedEntry> cache = new ConcurrentHashMap<>();
     Repository repository = null;
     private Long validity = null;
+    private boolean notifyOnChanges;
     
     public DatasourceConfigSource() {
         log.info("Loading [db] MicroProfile ConfigSource");
+        this.notifyOnChanges = loadNotifyOnChanges();
         super.initOrdinal(120);
     }
 
     @Override
     public Map<String, String> getPropertiesIfEnabled() {
-        if (repository == null) {
-            // late initialization is needed because of the EE datasource.
-            repository = new Repository(getConfig());
-        }
+        initRepository();
         return repository.getAllConfigValues();
     }
 
@@ -34,28 +37,73 @@ public class DatasourceConfigSource extends EnabledConfigSource {
         if (CONFIG_ORDINAL.equals(propertyName)) {
             return null;
         }
-        if (repository == null) {
-            // late initialization is needed because of the EE datasource.
-            repository = new Repository(getConfig());
-        }
-        if (validity == null) {
-            validity = getConfig().getOptionalValue("configsource.db.validity", Long.class).orElse(30000L);
-        }
+        
+        initRepository();
+        initValidity();
+        
+        // TODO: Cache null values ? So if the config is not in the DB, cache the empty value
+        // TODO: If not first time load, fire NEW Event ?
         TimedEntry entry = cache.get(propertyName);
-        if (entry == null || entry.isExpired()) {
-            log.log(Level.FINE, () -> "load " + propertyName + " from db");
-            String value = repository.getConfigValue(propertyName);
-            cache.put(propertyName, new TimedEntry(value));
-            return value;
+        
+        if (entry == null){
+            // First time read (Or no config)
+            return readFromDB(propertyName);
+        }else if(entry.isExpired()) {
+            // Time to refresh
+            ChangeEventNotifier changeEventNotifier = ChangeEventNotifier.getInstance();
+            String oldValue = entry.getValue();
+            String newValue = readFromDB(propertyName);
+            if(notifyOnChanges){
+                // Remove Event
+                if(newValue==null){
+                    changeEventNotifier.fire(new ChangeEvent(Type.REMOVE,propertyName,changeEventNotifier.getOptionalOldValue(oldValue),null,NAME));
+                // Change Event
+                }else if(!oldValue.equals(newValue)){
+                    changeEventNotifier.fire(new ChangeEvent(Type.UPDATE,propertyName,changeEventNotifier.getOptionalOldValue(oldValue),newValue,NAME));
+                }
+            }
+            
+            return newValue;
+        }else{
+            // Read from cache
+            return entry.getValue();
         }
-        return entry.getValue();
+        
     }
 
     @Override
     public String getName() {
-        return "DatasourceConfigSource";
+        return NAME;
     }
 
+    private String readFromDB(String propertyName){
+        log.log(Level.FINE, () -> "load " + propertyName + " from db");
+        String value = repository.getConfigValue(propertyName);
+        cache.put(propertyName, new TimedEntry(value));
+        return value;
+    }
+    
+    private void initRepository(){
+        if (repository == null) {
+            // late initialization is needed because of the EE datasource.
+            repository = new Repository(getConfig());
+        }
+    }
+    
+    private void initValidity(){
+        if (validity == null) {
+            validity = getConfig().getOptionalValue("configsource.db.validity", Long.class).orElse(30000L);
+        }
+    }
+    
+    private String getConfigKey(String subKey){
+        return "configsource.db." + subKey;
+    }
+    
+    private boolean loadNotifyOnChanges(){
+        return getConfig().getOptionalValue(getConfigKey(NOTIFY_ON_CHANGES), Boolean.class).orElse(DEFAULT_NOTIFY_ON_CHANGES);
+    }
+    
     class TimedEntry {
         private final String value;
         private final long timestamp;
@@ -73,4 +121,7 @@ public class DatasourceConfigSource extends EnabledConfigSource {
             return (timestamp + validity) < System.currentTimeMillis();
         }
     }
+    
+    private static final String NOTIFY_ON_CHANGES = "notifyOnChanges";
+    private static final boolean DEFAULT_NOTIFY_ON_CHANGES = true;
 }
