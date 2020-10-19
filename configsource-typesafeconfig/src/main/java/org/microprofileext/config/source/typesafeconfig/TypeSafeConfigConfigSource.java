7dcc2af3e0e9f60aec4e1d0b114ad7a99d76fea4
Chad Spray <cspray@fanthreesixty.com>
2020-10-19 11:17:43 -0500

    Fixes log statement

--- configsource-typesafeconfig/src/main/java/org/microprofileext/config/source/typesafeconfig/TypeSafeConfigConfigSource.java
+++ configsource-typesafeconfig/src/main/java/org/microprofileext/config/source/typesafeconfig/TypeSafeConfigConfigSource.java
@@ -67,8 +67,8 @@ public class TypeSafeConfigConfigSource extends EnabledConfigSource {
             log.log(Level.FINE, "load {0} from typesafe config", key);
             return config.getString(key);
         } catch (ConfigException ex) {
-            log.log(Level.WARNING, "Config key '{0}' could not be retrieved due to ConfigException with "
-                    + "message '{1}'. To see stack trace enable trace logging", new String[]{key, ex.getMessage()});
+            log.log(Level.WARNING, "Config key ''{0}'' could not be retrieved due to ConfigException with "
+                    + "message ''{1}''. To see stack trace enable trace logging", new String[]{key, ex.getMessage()});
             log.log(Level.FINER, "ConfigException stack trace", ex);
             return null;
         }
